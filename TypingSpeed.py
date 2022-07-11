d9fe72f7d42c671c2e54b6fe468f2897493023a8
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-07-11 10:59:09 +0530

    Update TypingSpeed.py

--- TypingSpeed.py
+++ TypingSpeed.py
@@ -1,7 +1,7 @@
 '''
 ~About Me:
     Hello World!
-    Creator: Sahil Rajwar Of Class 12th-A Science Student.
+    Creator: Sahil Rajwar Computer Science Student.
     Date: Thursday, 14/Oct/2021
 
 '''
